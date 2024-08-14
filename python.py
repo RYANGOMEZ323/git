96a25440a1f1e8576a94e6ccb5ff17d59aa87bee
lingesh1820 <youlingeshsakthivel1804@gmail.com>
2024-08-14 16:12:18 +0530

    lingesh

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ print("ryan gomez")
 a = 15
 print("ryan gomez")
 print("Hi Guys, I also joined in this project with you!")
-
+ 
 print("hello")
 print("ryan")
 print("lingeshwar")
@@ -12,3 +12,6 @@ print("lingeshwar")
 
 print("alan branch")
 print("lingesh branch")
+print("ryan branch")
+print("alan new branch")
+print("lingesh new branch")
