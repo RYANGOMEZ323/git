9a98bf240eeae50bb10dd3fd292e90c0a6af7e9f
Your Name <ealanraj1472@gmail.com>
2024-08-14 16:04:20 +0530

    alan new branch

--- python.py
+++ python.py
@@ -4,10 +4,13 @@ print("ryan gomez")
 a = 15
 print("ryan gomez")
 print("Hi Guys, I also joined in this project with you!")
-
+ 
 print("hello")
 print("ryan")
-
+print("lingeshwar")
 
 
 print("alan branch")
+print("lingesh branch")
+print("ryan branch")
+print("alan new branch")
